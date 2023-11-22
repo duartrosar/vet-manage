a1e8061b26e75d253ef0a1bb1ce99bc028345370
duartrosar <duartrosar@gmail.com>
2023-11-22 10:42:07 +0000

    Fix broken import

--- app/app/owners/OwnersHome.tsx
+++ app/app/owners/OwnersHome.tsx
@@ -1,7 +1,7 @@
 import Container from "@/components/container";
 import Link from "next/link";
 import React from "react";
-import { IoPencil, IoPersonAdd, IoTrash } from "react-icons/io5";
+import { IoPencil, IoPersonAdd, IoSearch, IoTrash } from "react-icons/io5";
 
 export default function OwnersHome() {
   return (
