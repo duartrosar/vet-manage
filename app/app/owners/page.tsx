7f075303433dafa9b838367ea9460b7959ad6ecc
duartrosar <duartrosar@gmail.com>
2023-12-13 09:10:52 +0000

    Trying to debug stuff in production

--- app/app/owners/page.tsx
+++ app/app/owners/page.tsx
@@ -1,10 +1,12 @@
 import Container from "@/components/container";
 import OwnersList from "@/components/list/owner/list";
-import prisma from "@/lib/db/prisma";
+import { getOwners } from "@/lib/db";
+// import prisma from "@/lib/db/prisma";
 
 export default async function OwnersHome() {
-  // const { owners } = await getOwners();
-  const owners = await prisma.owner.findMany();
+  const { owners } = await getOwners();
+  // TODO: Fix this to make it more safe
+  // const owners = await prisma.owner.findMany();
 
   return (
     <Container>
