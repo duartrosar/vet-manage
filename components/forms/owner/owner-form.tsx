0fe5b971f8e2c1e60f7723a81199aad2a82facd0
Duarte Ribeiro <duartrosar@gmail.com>
2024-01-07 02:45:39 +0000

    feat(form): implement new form using shadcn form component
    Add shadcn form component and create a reusable TextInput component that
    uses shadcn FormItem and Input.

--- components/forms/owner/owner-form.tsx
+++ components/forms/owner/owner-form.tsx
@@ -4,7 +4,6 @@ import React, { useEffect, useState } from "react";
 import { useFormStatus } from "react-dom";
 import { createOwnerWithUser, getUser, updateOwner } from "../../../lib/db";
 import ImageSelector from "../inputs/image-selector";
-import Input from "../inputs/input";
 import Selector from "../inputs/selector";
 import { SubmitHandler, useForm } from "react-hook-form";
 import { Owner } from "@prisma/client";
@@ -22,6 +21,16 @@ import {
 import { setOwnerFormIsOpen } from "@/lib/redux/slices/form-slice";
 import { toast } from "sonner";
 import Toast from "@/components/toast/toasters";
+import {
+  Form,
+  FormControl,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from "@/components/ui/form";
+import { Input } from "@/components/ui/input";
+import TextInput from "../inputs/text-input";
 
 export default function OwnerForm({ ownerId }: { ownerId?: number }) {
   const owner = useAppSelector((state) => state.form.owner);
@@ -31,15 +40,7 @@ export default function OwnerForm({ ownerId }: { ownerId?: number }) {
   const [file, setFile] = useState<File>();
   const options = genderOptions;
 
-  const {
-    register,
-    handleSubmit,
-    watch,
-    reset,
-    setValue,
-    formState: { errors, isSubmitting },
-    clearErrors,
-  } = useForm<Owner>({
+  const form = useForm<Owner>({
     defaultValues: {
       id: 0,
       firstName: "",
@@ -55,14 +56,14 @@ export default function OwnerForm({ ownerId }: { ownerId?: number }) {
   });
 
   useEffect(() => {
-    reset();
+    form.reset();
 
     if (owner) {
       setValues(owner);
     }
   }, []);
 
-  const processForm: SubmitHandler<Owner> = async (data: Owner) => {
+  const onSubmit: SubmitHandler<Owner> = async (data: Owner) => {
     // TODO: Uncomment this
     // if (file) {
     //   data.imageUrl = await blobUpload();
@@ -133,38 +134,56 @@ export default function OwnerForm({ ownerId }: { ownerId?: number }) {
   };
 
   function setValues(owner: Owner) {
-    setValue("id", owner.id);
-    setValue("firstName", owner.firstName);
-    setValue("lastName", owner.lastName);
-    setValue("dateOfBirth", owner.dateOfBirth);
-    setValue("email", owner.email);
-    setValue("mobileNumber", owner.mobileNumber);
-    setValue("gender", owner.gender);
-    setValue("address", owner.address);
-    setValue("imageUrl", owner.imageUrl);
+    form.setValue("id", owner.id);
+    form.setValue("firstName", owner.firstName);
+    form.setValue("lastName", owner.lastName);
+    form.setValue("dateOfBirth", owner.dateOfBirth);
+    form.setValue("email", owner.email);
+    form.setValue("mobileNumber", owner.mobileNumber);
+    form.setValue("gender", owner.gender);
+    form.setValue("address", owner.address);
+    form.setValue("imageUrl", owner.imageUrl);
   }
 
   return (
-    <form onSubmit={handleSubmit(processForm)} className="w-full p-4 xl:p-6 ">
-      <div className="space-y-3 lg:grid lg:grid-cols-3 lg:gap-3 lg:space-y-0">
-        <div className="">
-          <ImageSelector setFile={setFile} imageUrl={owner?.imageUrl} />
-        </div>
-        <div className="w-full md:space-y-3 lg:col-span-2">
-          <div className="w-full space-y-3 md:grid md:grid-cols-2 md:gap-3 md:space-y-0 xl:grid-cols-2">
-            <Input<Owner>
-              name="First Name"
-              register={register}
-              error={errors.firstName}
-            />
-            <Input<Owner>
-              name="Last Name"
-              register={register}
-              error={errors.lastName}
-            />
+    <Form {...form}>
+      <form
+        onSubmit={form.handleSubmit(onSubmit)}
+        className="w-full p-4 xl:p-6 "
+      >
+        <div className="space-y-3 lg:grid lg:grid-cols-3 lg:gap-3 lg:space-y-0">
+          <div className="">
+            <ImageSelector setFile={setFile} imageUrl={owner?.imageUrl} />
           </div>
-          <div className="w-full space-y-3 md:grid md:grid-cols-2 md:gap-3 md:space-y-0 xl:grid-cols-2">
-            <DatePicker
+          <div className="w-full md:space-y-3 lg:col-span-2">
+            <div className="w-full space-y-3 md:grid md:grid-cols-2 md:gap-3 md:space-y-0 xl:grid-cols-2">
+              <FormField
+                control={form.control}
+                name="firstName"
+                render={({ field }) => (
+                  <TextInput
+                    label="First Name"
+                    {...field}
+                    placeholder="First Name"
+                    error={form.formState.errors.firstName}
+                  />
+                )}
+              />
+              <FormField
+                control={form.control}
+                name="lastName"
+                render={({ field }) => (
+                  <TextInput
+                    label="Last Name"
+                    {...field}
+                    placeholder="Last Name"
+                    error={form.formState.errors.lastName}
+                  />
+                )}
+              />
+            </div>
+            <div className="w-full space-y-3 md:grid md:grid-cols-2 md:gap-3 md:space-y-0 xl:grid-cols-2">
+              {/* <DatePicker
               name="Date Of Birth"
               type="date"
               setSelectedOption={setValue}
@@ -182,54 +201,67 @@ export default function OwnerForm({ ownerId }: { ownerId?: number }) {
               error={errors.gender}
               clearErrors={clearErrors}
               options={options}
-            />
-          </div>
-          <div className="w-full space-y-3 md:grid md:grid-cols-2 md:gap-3 md:space-y-0 xl:grid-cols-2">
-            <span>
-              <Input<Owner>
+            /> */}
+            </div>
+            <div className="w-full space-y-3 md:grid md:grid-cols-2 md:gap-3 md:space-y-0 xl:grid-cols-2">
+              <span>
+                <FormField
+                  control={form.control}
+                  name="email"
+                  render={({ field }) => (
+                    <TextInput
+                      label="Email"
+                      {...field}
+                      placeholder="Email"
+                      error={form.formState.errors.email}
+                    />
+                  )}
+                />
+                {/* <Input<Owner>
                 // {owner && readonly}
                 readOnly={owner ? true : false}
                 name="Email"
                 type="email"
                 register={register}
                 error={errors.email}
-              />
-              {emailError && (
-                <span className="text-right text-xs font-bold text-red-500">
-                  {emailError}
-                </span>
-              )}
-            </span>
+              /> */}
+                {emailError && (
+                  <span className="text-right text-xs font-bold text-red-500">
+                    {emailError}
+                  </span>
+                )}
+              </span>
 
-            <Input<Owner>
+              {/* <Input<Owner>
               name="Mobile Number"
               type="tel"
               register={register}
               error={errors.mobileNumber}
-            />
+            /> */}
+            </div>
+            <div className="w-full space-y-3 md:grid md:grid-cols-2 md:gap-3 md:space-y-0 xl:grid-cols-2">
+              {/* TODO: Fix errors display */}
+              {/* <Address<Owner> register={register} error={errors.address} />
+            <Address<Owner> register={register} error={errors.address} /> */}
+            </div>
+            <div className="w-full space-y-3 md:grid md:grid-cols-2 md:gap-3 md:space-y-0 xl:grid-cols-2">
+              {/* <Address<Owner> register={register} error={errors.address} />
+            <Address<Owner> register={register} error={errors.address} /> */}
+            </div>
           </div>
-          <div className="w-full space-y-3 md:grid md:grid-cols-2 md:gap-3 md:space-y-0 xl:grid-cols-2">
-            {/* TODO: Fix errors display */}
-            <Address<Owner> register={register} error={errors.address} />
-            <Address<Owner> register={register} error={errors.address} />
-          </div>
-          <div className="w-full space-y-3 md:grid md:grid-cols-2 md:gap-3 md:space-y-0 xl:grid-cols-2">
-            <Address<Owner> register={register} error={errors.address} />
-            <Address<Owner> register={register} error={errors.address} />
+          <div className="col-start-2 gap-1 text-end lg:text-start">
+            <button
+              type="submit"
+              onClick={(e: React.FormEvent<HTMLButtonElement>) => {
+                if (pending) e.preventDefault;
+              }}
+              className="w-full rounded-lg border-2 border-cerulean-100/25 bg-cerulean-600 px-6 py-2 text-cerulean-100 hover:bg-cerulean-800 focus:border-cerulean-600 focus:outline-2 focus:outline-cerulean-600 lg:w-1/2"
+            >
+              {owner ? "Save owner" : "Create owner"}
+            </button>
           </div>
         </div>
-        <div className="col-start-2 gap-1 text-end lg:text-start">
-          <button
-            type="submit"
-            onClick={(e: React.FormEvent<HTMLButtonElement>) => {
-              if (pending) e.preventDefault;
-            }}
-            className="w-full rounded-lg border-2 border-cerulean-100/25 bg-cerulean-600 px-6 py-2 text-cerulean-100 hover:bg-cerulean-800 focus:border-cerulean-600 focus:outline-2 focus:outline-cerulean-600 lg:w-1/2"
-          >
-            {owner ? "Save owner" : "Create owner"}
-          </button>
-        </div>
-      </div>
-    </form>
+      </form>
+    </Form>
   );
 }
--- components/user-profile/index.tsx
+++ components/user-profile/index.tsx
@@ -27,7 +27,7 @@ export default function ProfileMenu({ user }: ProfileMenuProps) {
   return (
     <div className="">
       <Popover>
-        <PopoverTrigger className="border-cerulean-100/10 mr-3 flex h-11 w-11 items-center justify-center rounded-full border bg-transparent">
+        <PopoverTrigger className="mr-3 flex h-11 w-11 items-center justify-center rounded-full border border-cerulean-100/10 bg-transparent">
           {user.image ? (
             <Image
               className="flex-none rounded-full"
@@ -37,28 +37,28 @@ export default function ProfileMenu({ user }: ProfileMenuProps) {
               alt="Profile picture"
             />
           ) : (
-            <FaUser className="text-cerulean-500/50 h-[20px] w-[20px]" />
+            <FaUser className="h-[20px] w-[20px] text-cerulean-500/50" />
           )}
         </PopoverTrigger>
 
         <PopoverContent
           align="end"
-          className="border-cerulean-100/25 bg-cerulean-900 z-20 z-50 w-full rounded-lg border-2 px-0 pb-2 pt-6 text-sm"
+          className="z-50 w-full rounded-lg border-2 border-cerulean-100/25 bg-cerulean-900 px-0 pb-2 pt-6 text-sm"
         >
-          <div className="border-cerulean-100/25 border-b px-5 pb-5">
+          <div className="border-b border-cerulean-100/25 px-5 pb-5">
             <h2 className=" text-white">{user.name}</h2>
             <p className="text-gray-400">{user.email}</p>
           </div>
           <ul className="flex flex-col px-2 pt-2">
             <li
               onClick={(e) => handleClick(e)}
-              className="hover:bg-cerulean-800 w-full cursor-pointer rounded-lg px-3 py-2 text-gray-400 transition hover:text-gray-200 hover:shadow-md"
+              className="w-full cursor-pointer rounded-lg px-3 py-2 text-gray-400 transition hover:bg-cerulean-800 hover:text-gray-200 hover:shadow-md"
             >
               <Link href="/">Account Settings</Link>
             </li>
             <li
               onClick={() => signOut()}
-              className="hover:bg-cerulean-800 w-full cursor-pointer rounded-lg px-3 py-2 text-gray-400 transition hover:text-gray-200 hover:shadow-md"
+              className="w-full cursor-pointer rounded-lg px-3 py-2 text-gray-400 transition hover:bg-cerulean-800 hover:text-gray-200 hover:shadow-md"
             >
               Sign Out
             </li>
