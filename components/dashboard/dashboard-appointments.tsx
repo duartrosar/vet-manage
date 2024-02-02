478498b81656424cfe05a107e591583264853915
Duarte Ribeiro <duartrosar@gmail.com>
2024-02-02 21:31:38 +0000

    fix: AppointmentsChartDisplay import

--- components/dashboard/dashboard-appointments.tsx
+++ components/dashboard/dashboard-appointments.tsx
@@ -1,6 +1,6 @@
 import React from "react";
-import AppointmentsChartDisplay from "./appointments-chart-display";
 import { getAppoinments } from "@/lib/db/actions/appointment-actions";
+import AppointmentsChartDisplay from "./appointments-chart-display";
 
 export default async function DashboardAppointments() {
   const { appointments } = await getAppoinments();
@@ -15,30 +15,5 @@ export default async function DashboardAppointments() {
     );
   }
 
-  // const data = getAppoinmentsPerYear(appointments);
-
   return <AppointmentsChartDisplay appointments={appointments} />;
 }
-
-// function getAppoinmentsPerYear(appointments: Appointment[]) {
-//   const year = 2024;
-//   const appointmentsInYear = appointments
-//     .filter((appointment) => appointment.startTime.getFullYear() === year)
-//     .map((appointment) => appointment);
-
-//   const currentData = [];
-
-//   for (let i = 0; i <= 11; i++) {
-//     const appointment = appointmentsInYear
-//       .filter((appointment) => appointment.startTime.getMonth() === i)
-//       .map((appointment) => appointment).length;
-
-//     const dataEntry = {
-//       amount: appointment,
-//       month: MONTHS[i],
-//     };
-//     currentData.push(dataEntry);
-//   }
-
-//   return currentData;
-// }
